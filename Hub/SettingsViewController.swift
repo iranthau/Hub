7a286eb4f5e0a5db06e08ceb7daa0d0a69c115e5
Alexei Gudimenko <s3170637@student.rmit.edu.au>
2016-01-13 17:03:15 +1100

    updates to AppDataModel & Settings View Controller
    Revised cell UI design
    Added NSUserDefaults funcs to data model (will be written to db / Parse
    later)
    Fixed navbar colors
    Other fixes and updates

---
+++ Hub/SettingsViewController.swift
@@ -0,0 +1,47 @@
+//
+//  SettingsViewController.swift
+//  Hub
+//
+//  Created by Alexei Gudimenko on 11/01/2016.
+//  Copyright © 2016 88Software. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UITableViewController {
+
+    //Variables (properties, outlets, etc) begin here:
+    @IBOutlet weak var hideProfileSwitch: UISwitch!
+    
+    //Methods begin here:
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /*check the state of hideProfileSwitch here:*/
+        //hideProfileSwitch.on =
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    //WARNING: check this during testing, as this method may require additional overriding
+    // of the tableView() methods, as it's a prototype-intended method type. Static cells
+    // are designed in the storyboard, so the size may need to be set there in some way!!! - A.G. 11/01/16
+    //give the bottom cell a unique size to display multiple lines of text
+    override func tableView(tableView: UITableView,
+        heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+            if indexPath.section == 0 && indexPath.row == 9 {
+                return 87 //height of bottom cell with team statement
+            } else {
+                return super.tableView(tableView, heightForRowAtIndexPath: indexPath)
+            }
+    }
+    
+    //UI @IBAction methods live here:
+    @IBAction func hideProfileToggle(switchControl: UISwitch) {
+        
+    }
+}
