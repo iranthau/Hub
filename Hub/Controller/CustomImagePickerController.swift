614b8cf1eefbfb3af0fd2452e528899c1137ff29
Iran R <iranthau@gmail.com>
2016-04-29 00:48:01 +1000

    Refactor custom image picker controller

--- Hub/Controller/CustomImagePickerController.swift
+++ Hub/Controller/CustomImagePickerController.swift
@@ -1,16 +1,12 @@
-//
 //  CustomImagePickerController.swift
 //  Hub
-//
 //  Created by Alexei Gudimenko on 15/02/2016.
 //  Copyright © 2016 88Software. All rights reserved.
-//
-//  Sets the style of the image picker controller, so that the nav bar buttons
-//  are displayed in white font automatically
 
 import UIKit
 
 class CustomImagePickerController: UIImagePickerController {
+    //  Sets the nav bar buttons color to white when image picker is displayed
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return .LightContent
     }
